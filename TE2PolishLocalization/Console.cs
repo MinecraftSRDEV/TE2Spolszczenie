c494cbf2ef06c552fed86743a848018ad4b20f94
MinecraftSRDEV <minecraftsrgames@gmail.com>
2025-10-17 09:29:42 +0200

    initial

---
+++ TE2PolishLocalization/Console.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace TE2PolishLocalizationNamespace
+{
+    public class DebugConsole : MonoBehaviour
+    {
+        private bool showConsole = false;
+        private static List<string> consoleLog = new List<string>();
+        private static Vector2 scrollPosition = Vector2.zero;
+        private static float textRect = 0f;
+
+        private string lastLog = "";
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.BackQuote))
+            {
+                showConsole = !showConsole;
+            }
+        }
+
+        private void OnEnable()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
+
+        private void OnDisable()
+        {
+            Application.logMessageReceived -= HandleLog;
+        }
+
+        private void HandleLog(string logString, string stackTrace, LogType type)
+        {
+            switch (type)
+            {
+                case LogType.Log:
+                    {
+                        consoleLog.Add("INFO: " + logString);
+                        scrollPosition.y = textRect;
+                        break;
+                    }
+                case LogType.Warning:
+                    {
+                        consoleLog.Add("WARN: " + logString);
+                        scrollPosition.y = textRect;
+                        break;
+                    }
+                case LogType.Error:
+                    {
+                        consoleLog.Add("ERRO: " + logString);
+                        scrollPosition.y = textRect;
+                        break;
+                    }
+                case LogType.Exception:
+                    {
+                        consoleLog.Add("EXCP: " + logString);
+                        scrollPosition.y = textRect;
+                        break;
+                    }
+                default:
+                    {
+                        consoleLog.Add("INFO: " + logString);
+                        scrollPosition.y = textRect;
+                        break;
+                    }
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (!showConsole)
+            {
+                return;
+            }
+
+            GUI.Box(new Rect(10, 10, Screen.width - 20, Screen.height / 3), "Debug Console");
+
+            textRect = 0f;
+            scrollPosition = GUI.BeginScrollView(new Rect(15, 40, Screen.width - 30, Screen.height / 3 - 80), scrollPosition, new Rect(0, 0, Screen.width - 50, consoleLog.Count * 20));
+            for (int i = 0; i < consoleLog.Count; i++)
+            {
+                GUI.Label(new Rect(5, i * 20, Screen.width - 100, 20), consoleLog[i]);
+                textRect += (i * 20);
+            }
+            GUI.EndScrollView();
+        }
+    }
+}
---
+++ TE2PolishLocalization/Localization.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using NodeCanvas.Tasks.Actions;
+using System.Reflection;
+using ParadoxNotion.Serialization.FullSerializer;
+using Newtonsoft.Json;
+using UnityEngine.SceneManagement;
+
+namespace TE2PolishLocalizationNamespace
+{
+    public class Localize : MonoBehaviour
+    {
+        private string UnescapeString(string str)
+        {
+            return str
+                .Replace("\\n", "\n")
+                .Replace("\\r", "\r")
+                .Replace("\\t", "\t")
+                .Replace("\\\\", "\\");
+        }
+        private void TryToPatch()
+        {
+            try
+            {
+                Debug.Log("Langpack zawiera: " + Content.Data.Count);
+                Type locType = typeof(Localization);
+                FieldInfo fTable = locType.GetField("m_StringTable",
+                    BindingFlags.NonPublic | BindingFlags.Static);
+                var dict = (Dictionary<string, string[]>)fTable.GetValue(null);
+
+                Debug.Log($"[PL] Odnaleziono m_StringTable: {dict.Count} wpisów");
+
+                foreach (var kvp in dict)
+                {
+                    if (Content.Data.ContainsKey(kvp.Key))
+                    {
+                        try
+                        {
+                            kvp.Value[1] = UnescapeString(Content.Data[kvp.Key]);
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogError("Problem z nadpisaniem wartości");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Gra nie zawiera klucza:" + kvp.Key.ToString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("[PL] Błąd podczas dostępu do lokalizacji: " + e.ToString());
+            }
+        }
+
+        private void Start()
+        {
+            TryToPatch();
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                TryToPatch();
+            }
+        }
+
+        private Dictionary<string, string> texts = new Dictionary<string, string>();
+    }
+
+    public class CustomLocalization
+    {
+        public static void Load()
+        {
+            CustomLocalization.consoleObject = new GameObject("Console");
+            CustomLocalization.consoleObject.AddComponent<DebugConsole>();
+            UnityEngine.Object.DontDestroyOnLoad(CustomLocalization.consoleObject);
+
+            Debug.Log("Debug console loaded");
+
+            CustomLocalization.localizatorObject = new GameObject("Localizator");
+            CustomLocalization.localizatorObject.AddComponent<Localize>();
+            UnityEngine.Object.DontDestroyOnLoad(CustomLocalization.localizatorObject);
+
+            Debug.Log("Localization loaded");
+        }
+
+        private static GameObject consoleObject;
+        private static GameObject localizatorObject;
+    }
+}
